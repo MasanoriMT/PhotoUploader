f067ae9399a7e8ec34653efcacb49d0e8c8c18d7
matoh <m_matoh@iti.co.jp>
2015-04-22 19:07:35 +0900

    画像選択方式を２つ実装

--- app/src/main/java/iti/co/jp/photouploader/MainActivity.java
+++ app/src/main/java/iti/co/jp/photouploader/MainActivity.java
@@ -1,19 +1,17 @@
 package iti.co.jp.photouploader;
 
-import android.content.ContentResolver;
+import android.annotation.TargetApi;
 import android.content.Intent;
-import android.database.Cursor;
 import android.net.Uri;
-import android.nfc.Tag;
-import android.os.Environment;
-import android.provider.MediaStore;
+import android.os.Build;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.AndroidRuntimeException;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ListView;
+import android.view.View;
+import android.widget.Button;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -27,9 +25,7 @@ import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.logging.Logger;
 
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
@@ -40,7 +36,10 @@ import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+    private static final int REQUEST_PICK_CONTENT = 1;
+    private static final int REQUEST_KITKAT_PICK_CONTENT = 2;
 
     private RequestQueue mQueue;
 
@@ -49,19 +48,87 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // アップロード処理に用いるVolleyのQueue
         mQueue = Volley.newRequestQueue(this, new MultiPartStack(null, getAllAllowsSocketFactory()));
 
+        // 画像選択方法その１
+        // 画像選択アプリをIntent起動して結果を受け取る
+        // この方法だと１画像ずつしか選択できない
+        Button selectImages = (Button)findViewById(R.id.select_images);
+        selectImages.setOnClickListener(this);
+
+        // 画像選択方法その２
+        // 外部アプリ（ギャラリーアプリ）で複数選択した画像をIntentで渡してもらう
+        // この方法だとギャラリーアプリから当アプリを起動するので少々わかりづらい
         Intent intent = getIntent();
         String action = intent.getAction();
         String type = intent.getType();
-
         if (Intent.ACTION_SEND_MULTIPLE.equals(action) && type != null) {
             if (type.startsWith("image/")) {
                 handleSendMultipleImages(intent); // 送られて来た複数の画像を処理します
             }
         }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.select_images) {
+
+            // 画像選択アプリをIntent起動
+            //
+            // (Storage Access Framework に関する情報)
+            //   https://developer.android.com/guide/topics/providers/document-provider.html
+            //   http://www.gaprot.jp/pickup/android-4-4/vol3/
+            //
+
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT){
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent, REQUEST_PICK_CONTENT);
+            } else {
+                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("image/*");
+                startActivityForResult(intent, REQUEST_KITKAT_PICK_CONTENT);
+            }
+
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //
+        // Intent起動した画像選択アプリから結果を受け取る
+        //
+
+        if (data == null) {
+            return;
+        }
+
+        Uri uri = data.getData();
+
+        String path = ContentProviderUtil.getPath(this, uri);
+        if (path != null) {
+            doUpload(new File(path));
+        }
 
 
+        /*
+        if(requestCode == REQUEST_PICK_CONTENT) {
+            String path = getPath(this, uri);
+            if (path != null) {
+                doUpload(new File(path));
+            }
+        } else if (requestCode == REQUEST_KITKAT_PICK_CONTENT) {
+
+            String path = getPath(this, getResultChoosePictureUri(uri));
+            if (path != null) {
+                doUpload(new File(path));
+            }
+
+        }
+        */
     }
 
     @Override
@@ -87,49 +154,36 @@ public class MainActivity extends ActionBarActivity {
     }
 
     private void handleSendMultipleImages(Intent intent) {
+
+        //
+        // ACTION_SEND_MULTIPLEで渡されたコンテンツを処理する
+        //
+
         ArrayList<Uri> imageUris = intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
         if (imageUris != null) {
-            List<ListItem> list = new ArrayList<>();
             for(Uri uri : imageUris) {
-                ListItem item = new ListItem();
-                item.setText(uri.getPath());
-                list.add(item);
-
-                // Uri → File
-                //
-                //   http://www.united-bears.co.jp/blog/archives/2336
-                //
-                String scheme = uri.getScheme();
-                String path = null;
-                if ("file".equals(scheme)) {
-                    path = uri.getPath();
-                } else if("content".equals(scheme)) {
-                    ContentResolver contentResolver = this.getContentResolver();
-                    Cursor cursor = contentResolver.query(uri, new String[] { MediaStore.MediaColumns.DATA }, null, null, null);
-                    if (cursor != null) {
-                        cursor.moveToFirst();
-                        path = cursor.getString(0);
-                        cursor.close();
-                    }
+                String path = ContentProviderUtil.getPath(this, uri);
+                if (path != null) {
+                    // アップロードを実行
+                    doUpload(new File(path));
                 }
-
-                doUpload(new File(path));
-
             }
 
-            MyArrayAdapter adapter = new MyArrayAdapter(this, 0, list);
-
-            ListView lv = (ListView) findViewById(R.id.list_view);
-            lv.setAdapter(adapter);
 
         }
     }
 
     private void doUpload(File file) {
-        Map<String,String> stringMap = new HashMap<String, String>();
+
+        //
+        // 指定されたファイルをサーバにアップロードする
+        // TODO:接続先サーバは要調整
+        //
+
+        Map<String,String> stringMap = new HashMap<>();
         stringMap.put("text", "hogege"); // ここは必要に応じて追加すればよい
 
-        Map<String,File> fileMap = new HashMap<String, File>();
+        Map<String,File> fileMap = new HashMap<>();
         fileMap.put("file", file);
 
         MultipartRequest multipartRequest = new MultipartRequest(
@@ -155,6 +209,22 @@ public class MainActivity extends ActionBarActivity {
         mQueue.add(multipartRequest);
     }
 
+
+    //
+    // KITKATからは永続的パーミッションを得る必要がある
+    //
+    //     http://blog.kotemaru.org/2014/11/23/android-choose-picture.html
+    //
+    // ただし、今回のサンプルアプリでは、毎回画像選択アプリからUriを受け取るので、永続的パーミッションを取得する必要はない
+    // 一度受け取ったUriのコンテンツを、後になって利用したい場合は、永続的パーミッションを取得しておかないと、利用できなくなるようだ
+    //
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    private Uri getResultChoosePictureUri(Uri uri) {
+        final int takeFlags = Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION;
+        getContentResolver().takePersistableUriPermission(uri, takeFlags);
+
+        return uri;
+    }
 
     /*
         オレオレ証明書サイト用
